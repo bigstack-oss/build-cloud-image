3682f104bc073390091497652e43a73759459550
Roy Tan <roy.tan@bigstack.co>
2024-06-20 12:57:18 +0800

    fix CVE-2024-1086 & disable kernel auto-upgrade

--- create-ubuntu.sh
+++ create-ubuntu.sh
@@ -2,43 +2,60 @@
 echo "------ creating ubuntu 20.04 LTS ------"
 rm -rf ./images/ubuntu2004.qcow2
 wget https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img -O ./images/ubuntu2004.qcow2
+echo "------ fix CVE-2024-1086 ------"
+qemu-img resize ./images/ubuntu2004.qcow2 +1.5G
+sudo virt-customize --run-command "growpart /dev/sda 1" -a ./images/ubuntu2004.qcow2
+sudo virt-customize --run-command "apt install -y linux-headers-5.15.0-107-generic linux-image-5.15.0-107-generic" -a ./images/ubuntu2004.qcow2
+echo "------ install pkg ------"
 sudo virt-customize --install nfs-common --selinux-relabel -a ./images/ubuntu2004.qcow2
 sudo virt-customize --install cifs-utils --selinux-relabel -a ./images/ubuntu2004.qcow2
 sudo virt-customize --install qemu-guest-agent --selinux-relabel -a ./images/ubuntu2004.qcow2
+echo "------ customize settings ------"
 sudo virt-customize --run-command "mkdir /etc/docker" -a ./images/ubuntu2004.qcow2
 cp ./config/ubuntu/getty-u20 ./config/ubuntu/getty@tty1.service
 cp ./config/ubuntu/cloud-u20.cfg ./config/ubuntu/cloud.cfg
 sudo virt-copy-in './config/ubuntu/getty@tty1.service' /lib/systemd/system/ -a ./images/ubuntu2004.qcow2
 sudo virt-copy-in './config/ubuntu/cloud.cfg' /etc/cloud/ -a ./images/ubuntu2004.qcow2
 sudo virt-copy-in './config/daemon.json' /etc/docker/ -a ./images/ubuntu2004.qcow2
+echo "------ disable kernel auto update ------"
+sudo virt-copy-in './config/ubuntu/20auto-upgrades' /etc/apt/apt.conf.d/ -a ./images/ubuntu2004.qcow2
+echo "------ prepare disk ------"
 sudo virt-sysprep -a ./images/ubuntu2004.qcow2
 
 echo "------ creating ubuntu 22.04 LTS ------"
 rm -rf ./images/ubuntu2204.qcow2
 wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img -O ./images/ubuntu2204.qcow2
+echo "------ install pkg ------"
 sudo virt-customize --install nfs-common --selinux-relabel -a ./images/ubuntu2204.qcow2
 sudo virt-customize --install cifs-utils --selinux-relabel -a ./images/ubuntu2204.qcow2
 sudo virt-customize --install qemu-guest-agent --selinux-relabel -a ./images/ubuntu2204.qcow2
+echo "------ customize settings ------"
 sudo virt-customize --run-command "mkdir /etc/docker" -a ./images/ubuntu2204.qcow2
 cp ./config/ubuntu/getty-u22 ./config/ubuntu/getty@tty1.service
 cp ./config/ubuntu/cloud-u22.cfg ./config/ubuntu/cloud.cfg
 sudo virt-copy-in './config/ubuntu/getty@tty1.service' /lib/systemd/system/ -a ./images/ubuntu2204.qcow2
 sudo virt-copy-in './config/ubuntu/cloud.cfg' /etc/cloud/ -a ./images/ubuntu2204.qcow2
 sudo virt-copy-in './config/daemon.json' /etc/docker/ -a ./images/ubuntu2204.qcow2
+echo "------ disable kernel auto update ------"
+sudo virt-copy-in './config/ubuntu/20auto-upgrades' /etc/apt/apt.conf.d/ -a ./images/ubuntu2204.qcow2
 sudo virt-sysprep -a ./images/ubuntu2204.qcow2
 
 echo "------ creating ubuntu 24.04 LTS ------"
 rm -rf ./images/ubuntu2404.qcow2
 wget https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img -O ./images/ubuntu2404.qcow2
+echo "------ install pkg ------"
 sudo virt-customize --install nfs-common --selinux-relabel -a ./images/ubuntu2404.qcow2
 sudo virt-customize --install cifs-utils --selinux-relabel -a ./images/ubuntu2404.qcow2
 sudo virt-customize --install qemu-guest-agent --selinux-relabel -a ./images/ubuntu2404.qcow2
+echo "------ customize settings ------"
 sudo virt-customize --run-command "mkdir /etc/docker" -a ./images/ubuntu2404.qcow2
 cp ./config/ubuntu/getty-u24 ./config/ubuntu/getty@tty1.service
 cp ./config/ubuntu/cloud-u24.cfg ./config/ubuntu/cloud.cfg
 sudo virt-copy-in './config/ubuntu/getty@tty1.service' /lib/systemd/system/ -a ./images/ubuntu2404.qcow2
 sudo virt-copy-in './config/ubuntu/cloud.cfg' /etc/cloud/ -a ./images/ubuntu2404.qcow2
-sudo virt-copy-in './config/daemon.json' /etc/docker/ -a ./images/ubuntu2404.qcow
+sudo virt-copy-in './config/daemon.json' /etc/docker/ -a ./images/ubuntu2404.qcow2
+echo "------ disable kernel auto update ------"
+sudo virt-copy-in './config/ubuntu/20auto-upgrades' /etc/apt/apt.conf.d/ -a ./images/ubuntu2404.qcow2
 sudo virt-sysprep -a ./images/ubuntu2404.qcow2
 
 echo "------ Done ------"
