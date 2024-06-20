da62d43233e96f4108f026f048b79333eaca733d
Roy Tan <roy.tan@bigstack.co>
2024-06-20 13:24:45 +0800

    fix CVE-2024-1086 & disable kernel auto-upgrade

--- create-debian.sh
+++ create-debian.sh
@@ -2,30 +2,38 @@
 echo "------ creating debian 10 buster ------"
 rm -rf ./images/debian-10-buster.qcow2
 wget https://cloud.debian.org/images/cloud/buster/latest/debian-10-genericcloud-amd64.qcow2 -O ./images/debian-10-buster.qcow2
+echo "------ install pkg ------"
 sudo virt-customize --install nfs-common --selinux-relabel -a ./images/debian-10-buster.qcow2
 sudo virt-customize --install cifs-utils --selinux-relabel -a ./images/debian-10-buster.qcow2
 sudo virt-customize --install qemu-guest-agent --selinux-relabel -a ./images/debian-10-buster.qcow2
+echo "------ customize settings ------"
 sudo virt-customize --run-command "mkdir /etc/docker" -a ./images/debian-10-buster.qcow2
 cp ./config/debian/getty-d10 ./config/debian/getty@tty1.service
 cp ./config/debian/cloud-d10.cfg ./config/debian/cloud.cfg
 sudo virt-copy-in './config/debian/getty@tty1.service' /lib/systemd/system/ -a ./images/debian-10-buster.qcow2
 sudo virt-copy-in './config/debian/cloud.cfg' /etc/cloud/ -a ./images/debian-10-buster.qcow2
 sudo virt-copy-in './config/daemon.json' /etc/docker/ -a ./images/debian-10-buster.qcow2
+echo "------ disable kernel auto update ------"
+sudo virt-copy-in './config/ubuntu/20auto-upgrades' /etc/apt/apt.conf.d/ -a ./images/debian-10-buster.qcow2
 sudo virt-sysprep -a ./images/debian-10-buster.qcow2
 echo "------ Done ------"
 
 echo "------ creating debian 11 bulleye ------"
 rm -rf ./images/debian-11-bulleye.qcow2
 wget https://cloud.debian.org/images/cloud/bullseye/latest/debian-11-genericcloud-amd64.qcow2 -O ./images/debian-11-bulleye.qcow2
+echo "------ install pkg ------"
 sudo virt-customize --install nfs-common --selinux-relabel -a ./images/debian-11-bulleye.qcow2
 sudo virt-customize --install cifs-utils --selinux-relabel -a ./images/debian-11-bulleye.qcow2
 sudo virt-customize --install qemu-guest-agent --selinux-relabel -a ./images/debian-11-bulleye.qcow2
+echo "------ customize settings ------"
 sudo virt-customize --run-command "mkdir /etc/docker" -a ./images/debian-11-bulleye.qcow2
 cp ./config/debian/getty-d11 ./config/debian/getty@tty1.service
 cp ./config/debian/cloud-d11.cfg ./config/debian/cloud.cfg
 sudo virt-copy-in './config/debian/getty@tty1.service' /lib/systemd/system/ -a ./images/debian-11-bulleye.qcow2
 sudo virt-copy-in './config/debian/cloud.cfg' /etc/cloud/ -a ./images/debian-11-bulleye.qcow2
 sudo virt-copy-in './config/daemon.json' /etc/docker/ -a ./images/debian-11-bulleye.qcow2
+echo "------ disable kernel auto update ------"
+sudo virt-copy-in './config/ubuntu/20auto-upgrades' /etc/apt/apt.conf.d/ -a ./images/debian-11-bulleye.qcow2
 sudo virt-sysprep -a ./images/debian-11-bulleye.qcow2
 echo "------ Done ------"
 
@@ -33,14 +41,18 @@ echo "------ Done ------"
 echo "------ creating debian 12 bookworm ------"
 rm -rf ./images/debian-12-bookworm.qcow2
 wget https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-genericcloud-amd64.qcow2 -O ./images/debian-12-bookworm.qcow2
+echo "------ install pkg ------"
 sudo virt-customize --install nfs-common --selinux-relabel -a ./images/debian-12-bookworm.qcow2
 sudo virt-customize --install cifs-utils --selinux-relabel -a ./images/debian-12-bookworm.qcow2
 sudo virt-customize --install qemu-guest-agent --selinux-relabel -a ./images/debian-12-bookworm.qcow2
+echo "------ customize settings ------"
 sudo virt-customize --run-command "mkdir /etc/docker" -a ./images/debian-12-bookworm.qcow2
 cp ./config/debian/getty-d12 ./config/debian/getty@tty1.service
 cp ./config/debian/cloud-d12.cfg ./config/debian/cloud.cfg
 sudo virt-copy-in './config/debian/getty@tty1.service' /lib/systemd/system/ -a ./images/debian-12-bookworm.qcow2
 sudo virt-copy-in './config/debian/cloud.cfg' /etc/cloud/ -a ./images/debian-12-bookworm.qcow2
 sudo virt-copy-in './config/daemon.json' /etc/docker/ -a ./images/debian-12-bookworm.qcow2
+echo "------ disable kernel auto update ------"
+sudo virt-copy-in './config/ubuntu/20auto-upgrades' /etc/apt/apt.conf.d/ -a ./images/debian-12-bookworm.qcow2
 sudo virt-sysprep -a ./images/debian-12-bookworm.qcow2
 echo "------ Done ------"
--- create-rocky.sh
+++ create-rocky.sh
@@ -3,30 +3,38 @@
 echo "------ creating Rocky 8 ------"
 rm -rf ./images/rocky8.qcow2
 wget https://download.rockylinux.org/pub/rocky/8/images/x86_64/Rocky-8-GenericCloud.latest.x86_64.qcow2 -O ./images/rocky8.qcow2
+echo "------ install pkg ------"
 sudo virt-customize --install nfs-utils --selinux-relabel -a ./images/rocky8.qcow2
 sudo virt-customize --install cifs-utils --selinux-relabel -a ./images/rocky8.qcow2
 sudo virt-customize --install qemu-guest-agent --selinux-relabel -a ./images/rocky8.qcow2
+echo "------ customize settings ------"
 sudo virt-customize --run-command "mkdir /etc/docker" -a ./images/rocky8.qcow2
 cp ./config/rocky/getty-r8 ./config/rocky/getty@tty1.service
 cp ./config/rocky/cloud-r8.cfg ./config/rocky/cloud.cfg
 sudo virt-copy-in './config/rocky/getty@tty1.service' /lib/systemd/system/ -a ./images/rocky8.qcow2
 sudo virt-copy-in './config/rocky/cloud.cfg' /etc/cloud/ -a ./images/rocky8.qcow2
 sudo virt-copy-in './config/daemon.json' /etc/docker/ -a ./images/rocky8.qcow2
+echo "------ disable kernel auto update ------"
+sudo virt-customize --run-command "echo 'exclude=kernel*' >> /etc/yum.conf" -a ./images/rocky8.qcow2
 sudo virt-sysprep -a ./images/rocky8.qcow2
 
 
 echo "------ creating Rocky 9 ------"
 rm -rf ./images/rocky9.qcow2
 wget https://download.rockylinux.org/pub/rocky/9/images/x86_64/Rocky-9-GenericCloud.latest.x86_64.qcow2 -O ./images/rocky9.qcow2
+echo "------ install pkg ------"
 sudo virt-customize --install nfs-utils --selinux-relabel -a ./images/rocky9.qcow2
 sudo virt-customize --install cifs-utils --selinux-relabel -a ./images/rocky9.qcow2
 sudo virt-customize --install qemu-guest-agent --selinux-relabel -a ./images/rocky9.qcow2
+echo "------ customize settings ------"
 sudo virt-customize --run-command "mkdir /etc/docker" -a ./images/rocky9.qcow2
 cp ./config/rocky/getty-r9 ./config/rocky/getty@tty1.service
 cp ./config/rocky/cloud-r9.cfg ./config/rocky/cloud.cfg
 sudo virt-copy-in './config/rocky/getty@tty1.service' /lib/systemd/system/ -a ./images/rocky9.qcow2
 sudo virt-copy-in './config/rocky/cloud.cfg' /etc/cloud/ -a ./images/rocky9.qcow2
 sudo virt-copy-in './config/daemon.json' /etc/docker/ -a ./images/rocky9.qcow2
+echo "------ disable kernel auto update ------"
+sudo virt-customize --run-command "echo 'exclude=kernel*' >> /etc/yum.conf" -a ./images/rocky9.qcow2
 sudo virt-sysprep -a ./images/rocky9.qcow2
 
 echo "------ Done ------"
