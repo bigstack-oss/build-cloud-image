b58cb4ae21927026cf1c6773e4a9399138e55b24
Roy Tan <yooniks9@gmail.com>
2025-03-21 12:59:17 +0800

    unlock centos7/8

--- create-centos.sh
+++ create-centos.sh
@@ -1,42 +1,42 @@
 
 #!/bin/bash
-#echo "------ creating centos 7 ------"
-#rm -rf ./images/CentOS-7.qcow2
-#wget https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2 -O ./images/CentOS-7.qcow2
-#echo "------ install pkg ------"
-#sudo virt-customize --install nfs-utils --selinux-relabel -a ./images/CentOS-7.qcow2
-#sudo virt-customize --install cifs-utils --selinux-relabel -a ./images/CentOS-7.qcow2
-#sudo virt-customize --install qemu-guest-agent --selinux-relabel -a ./images/CentOS-7.qcow2
-#echo "------ customize settings ------"
-#sudo virt-customize --run-command "mkdir /etc/docker" -a ./images/CentOS-7.qcow2
-#cp ./config/centos/getty-c7 ./config/centos/getty@tty1.service
-#cp ./config/centos/cloud-c7.cfg ./config/centos/cloud.cfg
-#sudo virt-copy-in './config/centos/getty@tty1.service' /lib/systemd/system/ -a ./images/CentOS-7.qcow2
-#sudo virt-copy-in './config/centos/cloud.cfg' /etc/cloud/ -a ./images/CentOS-7.qcow2
-#sudo virt-copy-in './config/daemon.json' /etc/docker/ -a ./images/CentOS-7.qcow2
-#echo "------ disable kernel auto update ------"
-#sudo virt-customize --run-command "echo 'exclude=kernel*' >> /etc/yum.conf" -a ./images/CentOS-7.qcow2
-#sudo virt-sysprep -a ./images/CentOS-7.qcow2
+echo "------ creating centos 7 ------"
+rm -rf ./images/CentOS-7.qcow2
+wget https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2 -O ./images/CentOS-7.qcow2
+echo "------ install pkg ------"
+sudo virt-customize --install nfs-utils --selinux-relabel -a ./images/CentOS-7.qcow2
+sudo virt-customize --install cifs-utils --selinux-relabel -a ./images/CentOS-7.qcow2
+sudo virt-customize --install qemu-guest-agent --selinux-relabel -a ./images/CentOS-7.qcow2
+echo "------ customize settings ------"
+sudo virt-customize --run-command "mkdir /etc/docker" -a ./images/CentOS-7.qcow2
+cp ./config/centos/getty-c7 ./config/centos/getty@tty1.service
+cp ./config/centos/cloud-c7.cfg ./config/centos/cloud.cfg
+sudo virt-copy-in './config/centos/getty@tty1.service' /lib/systemd/system/ -a ./images/CentOS-7.qcow2
+sudo virt-copy-in './config/centos/cloud.cfg' /etc/cloud/ -a ./images/CentOS-7.qcow2
+sudo virt-copy-in './config/daemon.json' /etc/docker/ -a ./images/CentOS-7.qcow2
+echo "------ disable kernel auto update ------"
+sudo virt-customize --run-command "echo 'exclude=kernel*' >> /etc/yum.conf" -a ./images/CentOS-7.qcow2
+sudo virt-sysprep -a ./images/CentOS-7.qcow2
 
 
-#echo "------ creating centos 8 ------"
-#rm -rf ./images/CentOS-Stream-8.qcow2
-#wget https://cloud.centos.org/centos/8-stream/x86_64/images/CentOS-Stream-GenericCloud-8-latest.x86_64.qcow2 -O ./images/CentOS-Stream-8.qcow2
-#echo "------ install pkg ------"
-#sudo virt-customize --install nfs-utils --selinux-relabel -a ./images/CentOS-Stream-8.qcow2
-#sudo virt-customize --install cifs-utils --selinux-relabel -a ./images/CentOS-Stream-8.qcow2
-#sudo virt-customize --install qemu-guest-agent --selinux-relabel -a ./images/CentOS-Stream-8.qcow2
-#echo "------ customize settings ------"
-#sudo virt-customize --run-command "mkdir /etc/docker" -a ./images/CentOS-Stream-8.qcow2
-#cp ./config/centos/getty-c8 ./config/centos/getty@tty1.service
-#cp ./config/centos/cloud-c8.cfg ./config/centos/cloud.cfg
-#sudo virt-copy-in './config/centos/getty@tty1.service' /lib/systemd/system/ -a ./images/CentOS-Stream-8.qcow2
-#sudo virt-copy-in './config/centos/cloud.cfg' /etc/cloud/ -a ./images/CentOS-Stream-8.qcow2
-#sudo virt-copy-in './config/daemon.json' /etc/docker/ -a ./images/CentOS-Stream-8.qcow2
-#echo "------ disable kernel auto update ------"
-#sudo virt-customize --run-command "echo 'exclude=kernel*' >> /etc/yum.conf" -a ./images/CentOS-Stream-8.qcow2
-#sudo virt-sysprep -a ./images/CentOS-Stream-8.qcow2
-#echo "------ Done ------"
+echo "------ creating centos 8 ------"
+rm -rf ./images/CentOS-Stream-8.qcow2
+wget https://cloud.centos.org/centos/8-stream/x86_64/images/CentOS-Stream-GenericCloud-8-latest.x86_64.qcow2 -O ./images/CentOS-Stream-8.qcow2
+echo "------ install pkg ------"
+sudo virt-customize --install nfs-utils --selinux-relabel -a ./images/CentOS-Stream-8.qcow2
+sudo virt-customize --install cifs-utils --selinux-relabel -a ./images/CentOS-Stream-8.qcow2
+sudo virt-customize --install qemu-guest-agent --selinux-relabel -a ./images/CentOS-Stream-8.qcow2
+echo "------ customize settings ------"
+sudo virt-customize --run-command "mkdir /etc/docker" -a ./images/CentOS-Stream-8.qcow2
+cp ./config/centos/getty-c8 ./config/centos/getty@tty1.service
+cp ./config/centos/cloud-c8.cfg ./config/centos/cloud.cfg
+sudo virt-copy-in './config/centos/getty@tty1.service' /lib/systemd/system/ -a ./images/CentOS-Stream-8.qcow2
+sudo virt-copy-in './config/centos/cloud.cfg' /etc/cloud/ -a ./images/CentOS-Stream-8.qcow2
+sudo virt-copy-in './config/daemon.json' /etc/docker/ -a ./images/CentOS-Stream-8.qcow2
+echo "------ disable kernel auto update ------"
+sudo virt-customize --run-command "echo 'exclude=kernel*' >> /etc/yum.conf" -a ./images/CentOS-Stream-8.qcow2
+sudo virt-sysprep -a ./images/CentOS-Stream-8.qcow2
+echo "------ Done ------"
 
 echo "------ creating centos 9 ------"
 rm -rf ./images/CentOS-Stream-9.qcow2
