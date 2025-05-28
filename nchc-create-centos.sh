5f5af1a92c644aac4e9286b3116c12f10432068f
Roy Tan <yooniks9@gmail.com>
2025-05-28 21:35:38 +0800

    centos10 nchc

--- nchc-create-centos.sh
+++ nchc-create-centos.sh
@@ -16,3 +16,19 @@ echo "------ prepare disk ------"
 sudo LIBGUESTFS_BACKEND=direct virt-sysprep -a ./images/centos_9-nchc.qcow2
 
 echo "------ Done ------"
+
+echo "------ creating centos_10------"
+cp ./images/centos_10.qcow2 ./images/centos_10-nchc.qcow2
+echo "------ remove auto-login ------"
+sudo LIBGUESTFS_BACKEND=direct virt-customize --run-command "rm -rf /lib/systemd/system/getty@tty1.service" -a ./images/centos_10-nchc.qcow2
+sudo LIBGUESTFS_BACKEND=direct virt-customize --run-command "sed -i '3iserver 140.110.16.1 iburst' /etc/chrony.conf"  -a ./images/centos_10-nchc.qcow2
+cp ./nchc/rhel/cloud.cfg-c10 ./nchc/rhel/cloud.cfg
+sudo LIBGUESTFS_BACKEND=direct virt-copy-in './nchc/rhel/cloud.cfg' /etc/cloud/ -a ./images/centos_10-nchc.qcow2
+cp ./nchc/rhel/centos-addons.repo-c10 ./nchc/rhel/centos-addons.repo
+sudo LIBGUESTFS_BACKEND=direct virt-copy-in "./nchc/rhel/centos-addons.repo" /etc/yum.repos.d/ -a ./images/centos_10-nchc.qcow2
+cp ./nchc/rhel/centos.repo-c10 ./nchc/rhel/centos.repo
+sudo LIBGUESTFS_BACKEND=direct virt-copy-in "./nchc/rhel/centos.repo" /etc/yum.repos.d/ -a ./images/centos_10-nchc.qcow2
+echo "------ prepare disk ------"
+sudo LIBGUESTFS_BACKEND=direct virt-sysprep -a ./images/centos_10-nchc.qcow2
+
+echo "------ Done ------"
